2872e88c8de283a1e7e37b87e0d353c847af779d
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-03-25 21:50:26 +0530

    feat: add resolvers

--- spotify-track/src/datasource/spotify-api.ts
+++ spotify-track/src/datasource/spotify-api.ts
@@ -1,12 +1,16 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
+import { Playlist } from "../types";
 
 export class SpotifyAPI extends RESTDataSource {
   baseURL = "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1/";
 
-  async getFeaturedPlaylists() {
-    const response = await this.get<{ playlists: { items: unknown[] } }>(
-      "browse/featured-playlists"
-    );
-    return response.playlists.items;
+  async getFeaturedPlaylists(): Promise<Playlist[]> {
+    const response = await this.get<{
+      playlists: {
+        items: Playlist[];
+      };
+    }>("browse/featured-playlists");
+
+    return response?.playlists?.items ?? [];
   }
 }
